6f5c6380288ec241016835dac2e700f3ff55b667
miracle11 <miraclelight11@gmail.com>
2023-07-25 20:45:59 +0100

    inital commit

---
+++ App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
